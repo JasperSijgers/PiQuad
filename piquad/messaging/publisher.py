295ea5674615d479c96bad41a7cbe4d4b69ddaa0
Jasper Sijgers <jasper@sijge.rs>
2022-10-31 22:32:19 +0100

    Removed all python code

--- piquad/messaging/publisher.py
+++
@@ -1,7 +0,0 @@
-class Publisher:
-
-    def __init__(self, topic):
-        self.topic = topic
-
-    def send_message(self, message):
-        self.topic.send_message(message)
--- piquad/motor/motor_location.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class MotorLocation(Enum):
-    BACK_RIGHT = 1
-    FRONT_RIGHT = 2
-    BACK_LEFT = 3
-    FRONT_LEFT = 4
-    ALL = 5
